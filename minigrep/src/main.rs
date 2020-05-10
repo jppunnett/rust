b689637ddd900d19a87ac83170f9d1dc125b41db
James Punnett <jppunnett@yahoo.com>
2020-05-10 15:06:08 +0000

    Assign args to vars.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}\nin file {}\n", query, filename);
 }
