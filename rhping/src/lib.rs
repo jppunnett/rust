80c202c125e06222dc5675b47abf4a6de40238a9
James Punnett <jppunnett@yahoo.com>
2020-11-28 08:06:19 -0400

    Change number of arguments.

--- rhping/src/lib.rs
+++ rhping/src/lib.rs
@@ -13,28 +13,27 @@ mod tests {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Main logic goes here.
+    println!("{:?}", config);
     Ok(())
 }
 
-
+#[derive(Debug)]
 pub struct Config {
     pub url: String,
     pub timeout: u32,
-    pub output_resp: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
 
         if args.len() < 2 {
-            return Err("Usage: rhping URL [-t=timeout] [-o]");
+            return Err("Usage: rhping URL [timeout]");
         }
 
         let url = args[1].clone();
-        let timeout = 0;
-        let output_resp = false;
+        let timeout = if args.len() > 2 { args[2].parse().unwrap() } else { 500 };
 
-        Ok(Config { url, timeout, output_resp })
+        Ok(Config { url, timeout })
     }
 }
 
