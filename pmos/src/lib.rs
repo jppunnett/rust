6121521bdb6398bd77a1427c4b17d3e1d3358f5e
James Punnett <jppunnett@yahoo.com>
2020-12-08 19:54:14 -0400

    Add command line args

--- pmos/src/lib.rs
+++ pmos/src/lib.rs
@@ -1,5 +1,15 @@
 use std::error::Error;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-	Ok(())
+#[derive(StructOpt)]
+#[structopt(about = "Create a photomosaic.")]
+pub struct Config {
+    /// Source image.
+    #[structopt(parse(from_os_str))]
+    pub source: PathBuf,
+}
+
+pub fn run(_config: Config) -> Result<(), Box<dyn Error>> {
+    Ok(())
 }
--- pmos/src/main.rs
+++ pmos/src/main.rs
@@ -1,7 +1,9 @@
+use structopt::StructOpt;
 use std::process;
 
 fn main() {
-    if let Err(e) = pmos::run() {
+	let config = pmos::Config::from_args();
+    if let Err(e) = pmos::run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
